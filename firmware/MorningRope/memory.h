50c076ea35792bd0015508ffc01ea3c2fcf75c10
Daniel Frenkel <daniel@valarsystems.com>
2025-11-26 07:06:02 -0800

    Initial commit

---
+++ firmware/MorningRope/memory.h
@@ -0,0 +1,73 @@
+#include <Preferences.h>
+
+bool stall_flag = false;
+
+bool stop_motor = false;
+bool opening_direction;
+
+int travel_distance;
+float diameter_pulley_cm = 1.2f;  // Only change the value if updating the pulley size
+
+
+float diameter_pulley_in = diameter_pulley_cm * 0.3937f;
+float circumference_cm = diameter_pulley_cm * PI;
+float circumference_in = diameter_pulley_in * PI;
+
+int current;
+int stall;
+int accel;
+int max_speed;
+int tcools = (3089838.00 * pow(float(max_speed), -1.00161534)) * 1.5;
+int motor_microsteps = 64;
+
+int set_zero = 0;  // Set to 1 to set home position
+bool run_motor = false;
+
+bool is_cm;
+
+bool is_closing;
+bool is_moving = false;
+bool stop_flag = false;
+bool overtemp_flag = false;
+
+uint8_t PWM_grad;
+uint32_t target_position;
+int32_t motor_position;
+uint32_t maximum_motor_position;
+uint8_t target_percent;
+
+int wifi_set;
+bool wifi_button = false;
+
+String ssid;
+String password;
+
+const byte DNS_PORT = 53;
+IPAddress apIP(192, 168, 4, 1);
+
+// Filter anti-rebond (debouncer)
+#define DEBOUNCE_TIME 250
+volatile uint32_t DebounceTimer = 0;
+
+Preferences preferences;
+
+void load_preferences() {
+
+  Serial.println("LOADING PREFERENCES");
+
+  wifi_set = preferences.getInt("wifi_set", 0);
+  ssid = preferences.getString("ssid", "NOT_SET");
+  password = preferences.getString("pass", "NOT_SET");
+  maximum_motor_position = preferences.getInt("max_motor_pos", 2695); // defaults to 20 inches
+  motor_position = preferences.getInt("motor_pos", 0);
+  current = preferences.getLong("current", 1000);
+  stall = preferences.getInt("stall", 10);
+  accel = preferences.getInt("accel", 10000);
+  max_speed = preferences.getInt("max_speed", 30000);
+  opening_direction = preferences.getInt("open_dir", 0);
+  is_cm = preferences.getInt("is_cm", 0);
+  travel_distance = preferences.getInt("travel_dist", 20);
+  target_percent = ((float)motor_position / (float)maximum_motor_position) * 100;
+
+  Serial.println("FINISHED LOADING PREFERENCES");
+}
---
+++ firmware/Simple/Simple.ino
@@ -0,0 +1,152 @@
+#include <TMCStepper.h>
+
+#define STEP_PIN 10
+
+#define ENABLE_PIN 8
+#define RX_PIN 5
+#define TX_PIN 6
+
+#define STALLGUARD_PIN 1
+#define INDEX_PIN 0
+
+#define BUTTON_1_PIN 3
+#define BUTTON_2_PIN 4
+#define BUTTON_WIFI_PIN 7
+
+#define R_SENSE 0.11f     // R_SENSE for current calc.
+#define DRIVER_ADDRESS 0  // TMC2209 Driver address according to MS1 and MS2
+
+
+
+// ## Speed
+// Sets the speed in microsteps per second.
+// If for example the the motor_microsteps is set to 16 and your stepper motor has 200 full steps per revolution (Most common type. The motor angle will be 1.8 degrees on the datasheet)
+// It means 200 x 16 = 3200 steps per revolution. To set the speed to rotate one revolution per seconds, we would set the value below to 3200.
+// µstep velocity v[Hz] = VACTUAL[2209] * 0.715Hz
+//Change these values to get different speeds
+#define CLOSE_VELOCITY 1200
+#define OPEN_VELOCITY -1200
+#define STOP_MOTOR_VELOCITY 0
+
+bool stalled_motor = false;
+bool motor_moving = false;
+bool is_moving = false;
+bool is_closing = false;
+bool move_to_max = false;
+
+uint32_t target_position;
+int32_t motor_position;
+uint32_t maximum_motor_position;
+uint8_t target_percent;
+
+// We communicate with the TMC2209 over UART
+// But the Arduino UNO only has one Serial port which is connected to the Serial Monitor
+// We can use software serial on the UNO, and hardware serial on the ESP32 or Mega 2560
+
+TMC2209Stepper driver(&Serial1, R_SENSE, DRIVER_ADDRESS);
+
+// Interrupt
+// This interrupt will fire when a HIGH rising signal is detected on the DIAG pin. This indicates a stall.
+
+void IRAM_ATTR stalled_position() {
+  stalled_motor = true;
+}
+
+// ## Track Steps
+// When using the pulse generator, the TMC2209 will tell us each time a step has been taken by pulsing the INDEX pin. We can create a tracker to add or subract steps from the tracker
+// The index output gives one pulse per electrical rotation, i.e., one pulse per each four fullsteps. I
+// The is_closing variable keeps track of which direction the motor is spinning, this way we know whether to add or subract from the position.
+
+void IRAM_ATTR index_interrupt(void) {
+
+  if (is_closing == true) {
+    motor_position++;
+  } else {
+    motor_position--;
+  }
+
+  // Ensure motor position stays within bounds
+  if (motor_position < 0) {
+    motor_position = 0;
+  } else if (motor_position > maximum_motor_position) {
+    motor_position = maximum_motor_position;
+  }
+}
+
+void setup() {
+  Serial.begin(115200);
+  Serial1.begin(115200, SERIAL_8N1, RX_PIN, TX_PIN);  // ESP32 can use any pins to Serial
+
+  Serial.println("START setup");
+  pinMode(STEP_PIN, OUTPUT);
+  pinMode(ENABLE_PIN, OUTPUT);
+
+  pinMode(STALLGUARD_PIN, INPUT);
+  pinMode(INDEX_PIN, INPUT);
+
+  attachInterrupt(digitalPinToInterrupt(STALLGUARD_PIN), stalled_position, RISING);
+  attachInterrupt(digitalPinToInterrupt(INDEX_PIN), index_interrupt, RISING);
+
+  driver.begin();  // Start all the UART communications functions behind the scenes
+
+
+  /* General Registers */
+  driver.I_scale_analog(false);
+  driver.internal_Rsense(false);
+  driver.en_spreadCycle(false);
+  driver.index_otpw(false);
+  driver.index_step(true);
+  driver.pdn_disable(true);
+  driver.mstep_reg_select(true);
+  driver.multistep_filt(true);
+
+  // NODECONF Registers
+  driver.senddelay(6);
+
+  // Factory Registers
+  driver.ottrim(0);
+
+  // Velocity Dependent Control
+  driver.irun(15);       // Max current. Based on 0.11 Rsense resistors
+  driver.ihold(0);
+  driver.iholddelay(1);  // Set I_HOLD_DELAY to 1 to 15 for smooth standstill current decay
+  driver.TPOWERDOWN(20);
+  driver.TPWMTHRS(0);
+  driver.VACTUAL(0);
+
+  // CHOPCONF – Chopper Configuration
+  driver.diss2vs(0);
+  driver.diss2g(0);
+  driver.dedge(0);
+  driver.intpol(1);
+
+  driver.mres(8);  // 8 = FULLSTEP mode. 200 pulses per revolution.
+  driver.vsense(0);
+  driver.tbl(2);
+  driver.hend(0);
+  driver.hstrt(4);
+  driver.toff(5);
+
+  // PWMCONF – Voltage PWM Mode StealthChop
+  driver.pwm_lim(12);
+  driver.pwm_reg(8);    // Try 2
+  driver.freewheel(1);  // 1= Freewheel mode. 3 = Coil Short HS. Only short the coil when motor is NOT moving. Use for physical security
+  driver.pwm_autograd(1);
+  driver.pwm_autoscale(1);
+  driver.pwm_freq(1);
+  //driver.pwm_grad(PWM_grad);  // Test different initial values. Use scope.
+  driver.pwm_ofs(36);
+  digitalWrite(ENABLE_PIN, LOW);
+  Serial.println("Setup Complete");
+
+  driver.VACTUAL(CLOSE_VELOCITY);
+
+}
+
+
+void loop() {
+  driver.VACTUAL(CLOSE_VELOCITY);
+  delay(4000);
+  driver.VACTUAL(OPEN_VELOCITY);
+  delay(4000);
+}
